9f9fd3bcc91855d9ff9c0596b2a13c94501455f6
aditya surabhi <aditya surabhi@10.0.0.174>
2020-02-17 23:32:41 -0600

    Comments Added

--- Assignment2/src/OceanExplorer.java
+++ Assignment2/src/OceanExplorer.java
@@ -13,13 +13,13 @@ import javafx.stage.Stage;
 import javafx.scene.input.KeyEvent;
 
 public class OceanExplorer extends Application{
-	final int dim = 10;
-    final int scl = 50;
-    final int island_count = 10;
+	final int dim = 10;						//dimensions
+    final int scl = 50;						//scale
+    final int island_count = 10;			
     OceanMap map = new OceanMap();
-    AnchorPane root ;
-    Scene scene;
-    boolean[][] grid = map.getMap();
+    AnchorPane root ;						// create object for anchorpane
+    Scene scene;							// create new scene
+    boolean[][] grid = map.getMap();		// grid
     Image ShipImage, Pirateship, MyIsland, PirateIsland;
     ImageView ShipImageView, PirateShipView, PirateShipView2, MyIslandView, PirateIslandView;
     int x = 6;
@@ -27,15 +27,15 @@ public class OceanExplorer extends Application{
     int x_cord = 4;
     int y_cord = 4;
     Ship ship = new Ship();
-    PirateShip pirateS_1 = new PirateShip(); // objects of pirateShip class
-    PirateShip pirateS_2 = new PirateShip();
+    PirateShip pirateS_1 = new PirateShip();	// object for pirateShip 1 class
+    PirateShip pirateS_2 = new PirateShip();	// object for pirateShip 2 class
     public static void main(String[] args) {
         launch(args);
     }
     public void start(Stage oceanStage) throws Exception {
         root = new AnchorPane();
         scene = new Scene(root,scl*dim,scl*dim);
-        Button button =  new Button("Reset");
+        Button button =  new Button("Reset");			// declare reset button
         for (int x = 0; x < dim; x++) {
             for (int y = 0; y < dim; y++) {
                 Rectangle rect = new Rectangle(x*scl, y*scl, scl, scl);
@@ -44,21 +44,21 @@ public class OceanExplorer extends Application{
                 root.getChildren().add(rect);
             }
         }
-        loadMyShipImage();
+        loadMyShipImage();					// calling loadMyShipImage Method
 		/*
 		 * button.setLayoutX(200); // setting x coordinate of the reset button
 		 * button.setLayoutY(500); // setting y coordinate of the reset button
 		 * root.getChildren().add(button); // adding button to the root
 		 */
-        loadPirateShip(x_cord,y_cord);
-        loadPirateShip2(x_cord+1,y_cord+1);
+        loadPirateShip(x_cord,y_cord);		// loadPirateShip
+        loadPirateShip2(x_cord+1,y_cord+1);	// loadPirateShip2
         for(int z=0; z<island_count; z++){
             int a = (int)(Math.random()*10);
             int b = (int)(Math.random()*10);
             if(a != x && b != y) {
                 if(a != x_cord && b != y_cord) {
                     if (a != (x_cord + 1) && b != (y_cord + 1)) {
-                        loadMyIsland(a, b);
+                        loadMyIsland(a, b);		// loadMyIsland method
                     }
                 }
             }
@@ -69,29 +69,31 @@ public class OceanExplorer extends Application{
             if(a != x && b != y) {
                 if(a != x_cord && b != y_cord) {
                     if (a != (x_cord + 1) && b != (y_cord + 1)) {
-                        loadPirateIsland(a, b);
+                        loadPirateIsland(a, b);		// loadPirateIsland method
                     }
                 }
             }
          }
          
-        oceanStage.setScene(scene);
-        oceanStage.setTitle("Christopher Columbus Game");
+        oceanStage.setScene(scene);							// attaching the scene
+        oceanStage.setTitle("Christopher Columbus Game");	// set title 
         ship.addObserver(pirateS_1);
         ship.addObserver(pirateS_2);
-        oceanStage.show();
-        startSailing();
-
+        oceanStage.show();								// showing the grid
+        startSailing();			// start game
     }
+    
+    // LoadMyShipImage: method to load ship image
     public void loadMyShipImage() throws Exception{
         ShipImage = new Image("ship.png",50,50,true,true);
         ShipImageView = new ImageView(ShipImage);
         map.setShipLocation(x,y);
-        ShipImageView.setX(x * scl);
-        ShipImageView.setY(y * scl);
+        ShipImageView.setX(x * scl);	// initial position on x-axis
+        ShipImageView.setY(y * scl);	// initial position on y-axis
         root.getChildren().add(ShipImageView);
-
     }
+    
+    // loadPirateShip: Method to load Pirate ship image
     public void loadPirateShip(int a  , int b) throws Exception{
         Pirateship = new Image("pirateShip.png",50,50,true,true);
         PirateShipView = new ImageView(Pirateship);
@@ -99,16 +101,18 @@ public class OceanExplorer extends Application{
         PirateShipView.setX(a* scl);
         PirateShipView.setY(b* scl);
         root.getChildren().add(PirateShipView);
-
     }
+    
+    // loadPirateShip2: Method to load another pirate ship
     public void loadPirateShip2(int a  , int b) throws Exception{
         PirateShipView2 = new ImageView(Pirateship);
         pirateS_2.setShipLocation(a*scl,b*scl);
         PirateShipView2.setX(a* scl);
         PirateShipView2.setY(b* scl);
         root.getChildren().add(PirateShipView2);
-
     }
+    
+    // loadMyIsland: method to load island
     public void loadMyIsland(int a, int b) throws Exception{
         MyIsland = new Image("island.jpg",50,50,true,true);
         MyIslandView = new ImageView(MyIsland);
@@ -116,8 +120,9 @@ public class OceanExplorer extends Application{
         MyIslandView.setX(a* scl);
         MyIslandView.setY(b* scl);
         root.getChildren().add(MyIslandView);
-
     }
+    
+    // loadPirateIsland: Method to load pirate island
 	private void loadPirateIsland(int a, int b) throws Exception { 
 		PirateIsland = new Image("PirateIsland.PNG", 50, 50, true, true);
 		PirateIslandView = new ImageView(PirateIsland);
@@ -126,12 +131,13 @@ public class OceanExplorer extends Application{
 		PirateIslandView.setY(b* scl);
 		root.getChildren().add(PirateIslandView);
 	 }
-    private void startSailing() {	//Method to  start Sailing
+	
+	// startSailing: method to start sailing
+    private void startSailing() {
         scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
 
-
             public void handle(KeyEvent key) {
-                switch(key.getCode()) {
+                switch(key.getCode()) {			// Switch statement
                     case RIGHT :
                         ship.goEast(map.getShipLocation().x*scl,map.getShipLocation().y*scl);
                         break;
--- Assignment2/src/OceanMap.java
+++ Assignment2/src/OceanMap.java
@@ -3,19 +3,20 @@ import java.awt.Point;
 public class OceanMap {
 	int x_cord;
 	int y_cord;
-	static boolean[][] grid = new boolean[10][10];
+	static boolean[][] grid = new boolean[10][10];		// initializing grid size
 
     public void setShipLocation(int i , int j){
         x_cord = i;
         y_cord = j;
     }
 
+    // getMap
     public boolean[][] getMap() {
         return grid;
     }
+    
+    // getShipLocation method
     public Point getShipLocation(){
         return new Point(x_cord,y_cord);
-
     }
-
 }
